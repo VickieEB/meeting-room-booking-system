67e9c9b04c3c6af576005ed7e6ccacdb7be3c049
VickieEB <victoria.etim.bassey@gmail.com>
2020-09-09 22:25:17 +0100

    Initial Project Add

---
+++ src/main/java/com/petproject/mrbs/MeetingRoomBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.mrbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petproject/mrbs/MeetingRoomBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.mrbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
