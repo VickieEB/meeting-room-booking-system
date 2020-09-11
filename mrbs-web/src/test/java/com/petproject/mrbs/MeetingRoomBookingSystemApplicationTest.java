4bed41834d4698914dd4b3064330ab5b0d64954e
VickieEB <victoria.etim.bassey@gmail.com>
2020-09-11 22:48:10 +0100

    Create multi-module project for data model and web. Closes #1

--- mrbs-web/src/test/java/com/petproject/mrbs/MeetingRoomBookingSystemApplicationTest.java
+++ mrbs-web/src/test/java/com/petproject/mrbs/MeetingRoomBookingSystemApplicationTest.java
@@ -3,11 +3,13 @@ package com.petproject.mrbs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class MeetingRoomBookingSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
