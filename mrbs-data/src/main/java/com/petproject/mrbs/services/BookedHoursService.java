a6f6d37c066d22f96e34363c78e99261faaa040a
VickieEB <victoria.etim.bassey@gmail.com>
2020-09-29 22:26:03 +0100

    Create Service Interfaces for Rooms and Booking. #7

---
+++ mrbs-data/src/main/java/com/petproject/mrbs/services/BookedHoursService.java
@@ -0,0 +1,6 @@
+package com.petproject.mrbs.services;
+
+import com.petproject.mrbs.domain.BookedHours;
+
+public interface BookedHoursService extends CrudService<BookedHours, Long>{
+}
---
+++ mrbs-data/src/main/java/com/petproject/mrbs/services/RoomService.java
@@ -0,0 +1,6 @@
+package com.petproject.mrbs.services;
+
+import com.petproject.mrbs.domain.Room;
+
+public interface RoomService extends CrudService<Room, Long> {
+}
