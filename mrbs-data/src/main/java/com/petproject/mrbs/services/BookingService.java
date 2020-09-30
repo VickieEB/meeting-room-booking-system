03e4f289864107dd304cbdfba69648e2b4b7bbb8
VickieEB <victoria.etim.bassey@gmail.com>
2020-09-30 08:25:15 +0100

    Create Service Interfaces for Rooms and Booking (Prefer this way). Closes #7

--- mrbs-data/src/main/java/com/petproject/mrbs/services/BookingService.java
+++ mrbs-data/src/main/java/com/petproject/mrbs/services/BookingService.java
@@ -1,36 +1,6 @@
 package com.petproject.mrbs.services;
 
-import com.petproject.mrbs.domain.Booking;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import java.awt.print.Book;
 
-import java.util.Set;
-
-@Slf4j
-@Service
-public class BookingService implements CrudService<Booking, Long>{
-    @Override
-    public Set<Booking> findAll() {
-        return null;
-    }
-
-    @Override
-    public Booking findById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public Booking save(Booking object) {
-        return null;
-    }
-
-    @Override
-    public void delete(Booking object) {
-
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-
-    }
+public interface BookingService extends CrudService<Book, Long>{
 }
