848b08266886d71b8dc9caa3de8ca1755ee91896
VickieEB <victoria.etim.bassey@gmail.com>
2020-10-08 22:16:49 +0100

    Create Rooms List and Controller. #12

---
+++ mrbs-web/src/main/java/com/petproject/mrbs/controller/RoomController.java
@@ -0,0 +1,24 @@
+package com.petproject.mrbs.controller;
+
+import com.petproject.mrbs.services.RoomService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rooms")
+public class RoomController {
+
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping({"/list", "/index", "/"})
+    public String listRooms(Model model){
+        model.addAttribute("rooms", roomService.findAll());
+        return "rooms/roomsList";
+    }
+}
